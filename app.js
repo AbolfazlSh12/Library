f0b0899ef2536b9a3a4f44c33312a57cb18aa175
AbolfazlSh12 <simple.learner.110@gmail.com>
2022-04-09 15:52:08 +0430

    mongodb installed

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
+var booksRouter = require('./routes/books')
 
 var app = express();
 
@@ -22,7 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter)
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+app.use('/books', booksRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Abolfazl' });
+  res.render('index', { title: 'Library' });
 });
 
 module.exports = router;
