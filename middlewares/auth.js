6611b5098463e1bd3e1c0216d6434a67b566664a
AbolfazlSh12 <simple.learner.110@gmail.com>
2022-05-11 11:54:09 +0430

    Fix Error and Set access for add book

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,20 +2,21 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 export const isAuthenticated = (req, res, next) => {
-  const token = req.cookies?.token;
-  if (!token) {
-    return res.status(403).send("Forbidden");
-  }
-
-  jwt.verify(
-    token.replace("Bearer ", ""),
-    process.env.JWT_SECRET,
-    (err, user) => {
-      if (err) {
-        return res.status(401).send("Unauthorized");
-      }
-      req.user = user;
-      return next();
+    const token = req.cookies ?.token;
+    if (!token) {
+        console.log("Token is : " , token);
+        return res.status(401).send("You'r not logged in !");
     }
-  );
+
+    jwt.verify(
+        token.replace("Bearer ", ""),
+        process.env.JWT_SECRET,
+        (err, user) => {
+            if (err) {
+                return res.status(400).send("Unauthorized");
+            }
+            req.user = user;
+            return next();
+        }
+    );
 };
--- routes/auth.js
+++ routes/auth.js
@@ -10,234 +10,238 @@ import { isAuthenticated } from "../middlewares/auth.js";
 import cookieParser from 'cookie-parser';
 authRouter.use(cookieParser());
 
-authRouter.get("/login", function (req, res, next) {
-  res.render("login");
+authRouter.get("/login", function(req, res, next) {
+    res.render("login");
 });
 
-authRouter.get("/signup", function (req, res, next) {
-  res.render("signup");
+authRouter.get("/signup", function(req, res, next) {
+    res.render("signup");
 });
 
-authRouter.get("/signup/verify", function (req, res, next) {
-  // res.render("verify");
-  // console.log(req.query.username);
-  // console.log(req.query.token);
-  const username = req.query.username;
-  const verifyEmailToken = req.query.token;
-  UserDataModel.findOne({ username, verifyEmailToken }).then(async (user) => {
-    if (!user) {
-      res.status(409).send("Incorrect Input");
-    } else {
-      user.isVerified = true;
-      await user.save();
-      res.redirect("/");
-    }
-  });
+authRouter.get("/signup/verify", function(req, res, next) {
+    // res.render("verify");
+    // console.log(req.query.username);
+    // console.log(req.query.token);
+    const username = req.query.username;
+    const verifyEmailToken = req.query.token;
+    UserDataModel.findOne({ username, verifyEmailToken }).then(async(user) => {
+        if (!user) {
+            res.status(409).send("Incorrect Input");
+        } else {
+            user.isVerified = true;
+            await user.save();
+            res.redirect("/");
+        }
+    });
 });
 
-authRouter.get("/login/forgetPassword", function (req, res, next) {
-  res.render("forgetPass");
+authRouter.get("/login/forgetPassword", function(req, res, next) {
+    res.render("forgetPass");
 });
 
-authRouter.get("/login/resetPassword", function (req, res, next) {
-  console.log("It's a reset password page");
-  res.render("resetPass");
+authRouter.get("/login/resetPassword", function(req, res, next) {
+    console.log("It's a reset password page");
+    res.render("resetPass");
 });
 
 const sendVerificationEmail = (address, code, username) => {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: true,
-    auth: {
-      user: "simple.learner.110@gmail.com",
-      pass: "LiveTheMomentNow",
-    },
-  });
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        // host: "smtp.gmail.com",
+        // port: 465,
+        // secure: true,
+        auth: {
+            user: "simple.learner.110@gmail.com",
+            pass: "LiveTheMomentNow",
+        },
+    });
 
-  var mailOptions = {
-    from: "simple.learner.110@gmail.com",
-    to: address,
-    subject: "Email verification From Library !",
-    text: `http://localhost:3000/auth/signup/verify?username=${username}&token=${code}`,
-  };
+    var mailOptions = {
+        from: "simple.learner.110@gmail.com",
+        to: address,
+        subject: "Email verification From Library !",
+        text: `http://localhost:3000/auth/signup/verify?username=${username}&token=${code}`,
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-      res.send("sent");
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+            res.send("sent");
+        }
+    });
 };
 
-authRouter.get("/changeRole", isAuthenticated, function (req, res) {
-  const token = req.cookies.token;
-  // console.log("Token is : ", token);
-  const decoded = jwt_decode(token);
-  console.log("Decoded token is : ", decoded);
-  if (decoded.username == process.env.SUPERUSER_USERNAME) {
-    res.render("changeRole");
-  } else {
-    return res.status(403).send("Forbidden Page !");
-  }
+authRouter.get("/changeRole", isAuthenticated, function(req, res) {
+    const token = req.cookies.token;
+    // console.log("Token is : ", token);
+    const decoded = jwt_decode(token);
+    // console.log("Decoded token is : ", decoded);
+    if (decoded.role == "owner") {
+        res.render("changeRole");
+    } else {
+        return res.status(403).send("Forbidden Page !");
+    }
 });
 
-authRouter.get("/logout", function (req, res, next) {
-  res.clearCookie("token");
-  res.redirect("/");
-  // res.end();
+authRouter.get("/logout", function(req, res, next) {
+    console.log(req.cookies);
+    req.cookies = null;
+    console.log(req.cookies);
+    res.clearCookie("token");
+    res.redirect("/");
+    // res.end();
 });
 
 /* Email Verification */
-authRouter.post("/signup", function (req, res, next) {
-  const { email, username } = req.body;
-  if (username == process.env.SUPERUSER_USERNAME) {
-    res.status(409).send("Duplicate username !");
-  } else {
-    const random = rand.generate(6);
-    sendVerificationEmail(email, random, username);
+authRouter.post("/signup", function(req, res, next) {
+    const { email, username } = req.body;
+    if (username == process.env.SUPERUSER_USERNAME) {
+        res.status(409).send("Duplicate username !");
+    } else {
+        const random = rand.generate(6);
 
-    const { name, family, password } = req.body;
-    const user = new UserDataModel({
-      name,
-      family,
-      username,
-      email,
-      password,
-      verifyEmailToken: random,
-    });
-    user
-      .save()
-      .then(async (user) => {
-        jwt.sign({ username }, process.env.JWT_SECRET, async (err, token) => {
-          await user.save();
-          return res.send(token);
+        const { name, family, password } = req.body;
+        const user = new UserDataModel({
+            name,
+            family,
+            username,
+            email,
+            password,
+            verifyEmailToken: random,
         });
-      })
-      .catch(() => {
-        res.status(409);
-        res.send("Signup error !");
-      });
-  }
+        user
+            .save()
+            .then(async(user) => {
+                const role = user.role;
+                jwt.sign({ username, role }, process.env.JWT_SECRET, async(err, token) => {
+                    await user.save();
+                    res.cookie("token", token);
+                    return res.send(token);
+                });
+                console.log("JWT Success !");
+                sendVerificationEmail(email, random, username);
+            })
+            .catch(() => {
+                res.status(409);
+                res.send("Signup error !");
+            });
+    }
 });
 
-authRouter.post("/login", function (req, res, next) {
-  const { username, password } = req.body;
+authRouter.post("/login", function(req, res, next) {
+    const { username, password } = req.body;
 
-  const ownerName = process.env.SUPERUSER_USERNAME;
-  const ownerPass = process.env.SUPERUSER_PASSWORD;
-  if (username === ownerName) {
-    if (password !== ownerPass) {
-      return res.status(404).send("not found");
-    } else {
-      jwt.sign(
-        { username, role: "owner" },
-        process.env.JWT_SECRET,
-        async (err, token) => {
-          // console.log(token);
-          res.cookie("token", token);
-          return res.send(token);
+    const ownerName = process.env.SUPERUSER_USERNAME;
+    const ownerPass = process.env.SUPERUSER_PASSWORD;
+    if (username === ownerName) {
+        if (password !== ownerPass) {
+            return res.status(404).send("not found");
+        } else {
+            jwt.sign({ username, role: "owner" },
+                process.env.JWT_SECRET,
+                async(err, token) => {
+                    // console.log(token);
+                    res.cookie("token", token);
+                    return res.send(token);
+                }
+            );
+            console.log(`Token is { ${req.cookies} }`);
         }
-      );
-      // console.log(`Token is { ${req.headers.cookie} }`);
     }
-  }
 
-  UserDataModel.findOne({ username, password })
-    .then(async (user) => {
-      const role = user.role;
-      if (!user) {
-        res.status(404).send("not found");
-      } else {
-        jwt.sign(
-          { username, role },
-          process.env.JWT_SECRET,
-          async (err, token) => {
-            res.cookie("token", token);
-            return res.send(token);
-          }
-        );
-        // console.log(`Token is { ${req.headers.cookie} }`);
-      }
-    })
-    .catch((err) => {
-      // console.log(err);
-      res.status(500).send();
-    });
+    UserDataModel.findOne({ username, password })
+        .then(async(user) => {
+            const role = user.role;
+            if (!user) {
+                res.status(404).send("not found");
+            } else {
+                jwt.sign({ username, role },
+                    process.env.JWT_SECRET,
+                    async(err, token) => {
+                        res.cookie("token", token);
+                        return res.send(token);
+                    }
+                );
+                // console.log(`Token is { ${req.headers.cookie} }`);
+            }
+        })
+        .catch((err) => {
+            // console.log(err);
+            res.status(500).send();
+        });
 });
 
 /* Forget Password */
-authRouter.post("/login/forgetPassword", function (req, res, next) {
-  const email = req.body.email;
-  // console.log(email);
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: true,
-    auth: {
-      user: "simple.learner.110@gmail.com",
-      pass: "LiveTheMomentNow",
-    },
-  });
+authRouter.post("/login/forgetPassword", function(req, res, next) {
+    const email = req.body.email;
+    // console.log(email);
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        // host: "smtp.gmail.com",
+        // port: 465,
+        // secure: true,
+        auth: {
+            user: "simple.learner.110@gmail.com",
+            pass: "LiveTheMomentNow",
+        },
+    });
 
-  var mailOptions = {
-    from: "simple.learner.110@gmail.com",
-    to: email,
-    subject: "Rest Password Email !",
-    text: "http://localhost:3000/auth/login/resetPassword",
-  };
+    var mailOptions = {
+        from: "simple.learner.110@gmail.com",
+        to: email,
+        subject: "Rest Password Email !",
+        text: "http://localhost:3000/auth/login/resetPassword",
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-      res.send("sent");
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+            res.send("sent");
+        }
+    });
 });
 
 /* Reset Password */
-authRouter.post("/login/resetPassword", function (req, res) {
-  const { username, password } = req.body;
+authRouter.post("/login/resetPassword", function(req, res) {
+    const { username, password } = req.body;
 
-  UserDataModel.findOne({ username }).then(async (user) => {
-    if (!user) {
-      res.status(404).send("Not found !");
-    } else {
-      user.password = password;
-      await user.save();
-      res.redirect("/");
-    }
-  });
+    UserDataModel.findOne({ username }).then(async(user) => {
+        if (!user) {
+            res.status(404).send("Not found !");
+        } else {
+            user.password = password;
+            await user.save();
+            res.redirect("/");
+        }
+    });
 });
 
 /* Set Role for users */
-authRouter.post("/changeRole", isAuthenticated, function (req, res) {
-  const index = req.headers.cookie.indexOf("token") + 6;
-  const token = req.headers.cookie.slice(index);
-  const decoded = jwt_decode(token);
-  if (decoded.role === "owner") {
-    const { username, role } = req.body;
-    UserDataModel.findOne({ username })
-      .then(async (user) => {
-        if (!user) {
-          res.status(404).send("not found");
-        } else {
-          console.log("Successfully changed role");
-          user.role = role;
-          await user.save();
-          res.end();
-          return;
-        }
-      })
-      .catch((err) => {
-        // console.log(err);
-        res.status(500).send();
-      });
-  }
+authRouter.post("/changeRole", isAuthenticated, function(req, res) {
+    const index = req.headers.cookie.indexOf("token") + 6;
+    const token = req.headers.cookie.slice(index);
+    const decoded = jwt_decode(token);
+    if (decoded.role === "owner") {
+        const { username, role } = req.body;
+        UserDataModel.findOne({ username })
+            .then(async(user) => {
+                if (!user) {
+                    res.status(404).send("not found");
+                } else {
+                    console.log("Successfully changed role");
+                    user.role = role;
+                    await user.save();
+                    res.end();
+                    return;
+                }
+            })
+            .catch((err) => {
+                // console.log(err);
+                res.status(500).send();
+            });
+    }
 });
